6d9a230d94876bfdc384b8feae2fe22bd2adf973
Victor Harbo Johnston <vhol@kb.dk>
2024-01-23 13:51:56 +0100

    add simple list extraction

--- src/main/java/dk/kb/util/YamlResolver.java
+++ src/main/java/dk/kb/util/YamlResolver.java
@@ -7,8 +7,9 @@ public class YamlResolver {
      * The path to the input YAML file.
      */
     String filePath;
+
     /**
-     * The type of YAML object to fetch from the file. Currently, supports: List, Map and Single-value.
+     * The type of YAML object to fetch from the file. Currently, supports: List, Sequence, Map and Single-value.
      */
     String yamlType;
 
@@ -16,14 +17,17 @@ public class YamlResolver {
      * If yamlType is Single-value, then this field should contain the YAML path to the value.
      */
     String singleValuePath;
+
     /**
-     * If yamlType is either List or Map, this field should contain the YAML path to the given list or map.
+     * If yamlType is either List, Sequence Map, this field should contain the YAML path to the given list or map.
      */
     String collectionPath;
+
     /**
      * If yamlType is Map or List, then this field should contain the key or entry-number to look for in the given map.
      */
     String key;
+
     /**
      * If an enum is to be created for the given values, set this to true.
      */
