12df3e6d9083f3d32edb7bc5178f5f6ce3f2a706
Victor Harbo Johnston <vhol@kb.dk>
2024-01-23 12:53:43 +0100

    Update testing and add documentation

--- src/main/java/dk/kb/util/InjectionMavenPlugin.java
+++ src/main/java/dk/kb/util/InjectionMavenPlugin.java
@@ -28,6 +28,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -62,46 +63,39 @@ public class InjectionMavenPlugin extends AbstractMojo{
      */
     public void execute() throws MojoExecutionException {
         for (YamlResolver yamlResolver : yamlResolvers) {
-            try {
-                if (yamlResolver.createEnum){
-                    String collectionEnum = getYamlValueAsEnumFromFile(yamlResolver);
+            if (yamlResolver.createEnum){
+                String collectionEnum = getYamlValueAsEnumFromFile(yamlResolver);
 
-                    getLog().info(collectionEnum.toString());
-                } else {
-                    // Resolve YAML
-                    YAML yaml = resolveLayeredConfigs(yamlResolver.filePath);
-                    // Flatten YAML to entries
-                    List<Map.Entry<String, Object>> flatYAML = YAMLUtils.flatten(yaml);
-                    // Filter YAML entries
-                    List<Map.Entry<String, Object>> nonNullYamlEntries = filterEntries(flatYAML);
-                    // Add YAML properties to Maven project properties
-                    addYamlPropertiesToProjectProperties(nonNullYamlEntries);
-                    getLog().info("Updated Maven project properties with " + nonNullYamlEntries.size() +
-                            " properties from: '" + yamlResolver.filePath);
-                }
-            } catch (IOException e) {
-                throw new MojoExecutionException("The MOJO could not be executed. YAML input file could probably not be loaded.");
+                getLog().info(collectionEnum.toString());
+            } else {
+                /*// Resolve YAML
+                YAML yaml = resolveLayeredConfigs(yamlResolver.filePath);
+                // Flatten YAML to entries
+                List<Map.Entry<String, Object>> flatYAML = YAMLUtils.flatten(yaml);
+                // Filter YAML entries
+                List<Map.Entry<String, Object>> nonNullYamlEntries = filterEntries(flatYAML);
+                // Add YAML properties to Maven project properties
+                addYamlPropertiesToProjectProperties(nonNullYamlEntries);
+                getLog().info("Updated Maven project properties with " + nonNullYamlEntries.size() +
+                        " properties from: '" + yamlResolver.filePath);*/
+                getLog().info("Not ENUM");
             }
         }
     }
 
+    /**
+     * Extract values from a list or map of values in a YAML file as an enum, that can be used as a maven property.
+     * @param yamlInput a configuration object containing information needed to extract the values from the YAML.
+     * @return a string ready to be used as multiple values in an ENUM in OpenAPI.
+     *          All values are concatenated by a comma.
+     */
     private String getYamlValueAsEnumFromFile(YamlResolver yamlInput) {
         getLog().info("Yaml entry is of type: " + yamlInput.yamlType);
 
         try {
-            getLog().info("Working... ");
-            YAML fullYaml = new YAML(yamlInput.filePath);
-
             switch (yamlInput.yamlType){
                 case "List":
-                    getLog().info("Yaml object is of type list");
-                    StringJoiner stringJoiner = new StringJoiner(", ");
-                    List<YAML> propValues = fullYaml.getYAMLList(yamlInput.collectionPath);
-                    for (YAML originYaml : propValues) {
-                        String enumEntry = originYaml.getString("[0]." + yamlInput.key);
-                        stringJoiner.add(enumEntry);
-                    }
-                    return stringJoiner.toString();
+                    return constructEnumFromList(yamlInput);
 
                 case "Map":
                     getLog().info("Yaml object is of type map");
@@ -113,11 +107,33 @@ public class InjectionMavenPlugin extends AbstractMojo{
                     throw new MojoFailureException("Wrong key has been used when configuring the plugin.");
             }
 
-        } catch (IOException | MojoFailureException e) {
+        } catch (MojoFailureException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Create a string from all entries in a YAML List. The strings are concatenated by ', ' and can be used as an
+     * ENUM value in an OpenAPI specification.
+     * @param yamlInput a configuration object containing information needed to extract the values from the YAML.
+     * @return all values from the given list as a comma seperated string.
+     */
+    private static String constructEnumFromList(YamlResolver yamlInput) {
+        try {
+            YAML fullYaml = new YAML(yamlInput.filePath);
+            StringJoiner stringJoiner = new StringJoiner(", ");
+            List<YAML> propValues = fullYaml.getYAMLList(yamlInput.collectionPath);
+            for (YAML originYaml : propValues) {
+                String enumEntry = originYaml.getString("[0]." + yamlInput.key);
+                stringJoiner.add(enumEntry);
+            }
+            return stringJoiner.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // ========================= OLD CODE =============================
     /**
      * Filters a list of parameters from a flattened YAML file.
      * @param flatYAML entries from a YAML file that have already been flattened.
